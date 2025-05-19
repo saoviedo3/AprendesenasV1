72bbfc266776e00277ce7ac85dc9d948714c503b
saoviedo3 <78757400+saoviedo3@users.noreply.github.com>
2025-05-18 19:38:52 -0500

    V1

---
+++ app/src/main/java/com/espe/aprendesenasv1/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.espe.aprendesenasv1
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.*
+import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+import androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.espe.aprendesenasv1.ui.theme.AprendesenasV1Theme
+import com.example.mlkitobjectdetection.GraphicOverlay
+import com.example.mlkitobjectdetection.ObjectAnalyzer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cameraExecutor: ExecutorService
+    //private lateinit var camera_capture_button: Button
+    private lateinit var viewFinder: PreviewView
+    private lateinit var graphicOverlay: GraphicOverlay
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+        graphicOverlay = findViewById(R.id.graphicOverlay)
+        viewFinder = findViewById(R.id.viewFinder)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+
+    @SuppressLint("LongLogTag")
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Preview
+            val preview = androidx.camera.core.Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, ObjectAnalyzer(graphicOverlay))
+                }
+            // Select back camera as a default
+            val cameraSelector = DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalyzer
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "Live Object Detector Sample App"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults) // Llamada al método de la clase padre
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/espe/aprendesenasv1/ObjectAnalyzer.kt
@@ -0,0 +1,79 @@
+package com.example.mlkitobjectdetection
+
+import android.annotation.SuppressLint
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.common.model.LocalModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
+
+
+class ObjectAnalyzer(graphicOverlay: GraphicOverlay) : ImageAnalysis.Analyzer {
+
+    val localModel = LocalModel.Builder()
+        .setAssetFilePath("model_meta.tflite")
+        .build()
+
+    // Live detection and tracking
+    val customObjectDetectorOptions =
+        CustomObjectDetectorOptions.Builder(localModel)
+            .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+            .enableMultipleObjects()
+            .enableClassification()
+            .setClassificationConfidenceThreshold(0.8f)
+            .setMaxPerObjectLabelCount(3)
+            .build()
+
+
+
+    val objectDetector = ObjectDetection.getClient(customObjectDetectorOptions)
+
+
+    val overlay = graphicOverlay
+    private val lensFacing = CameraSelector.LENS_FACING_BACK
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val isImageFlipped = lensFacing == CameraSelector.LENS_FACING_FRONT
+        val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+        if (rotationDegrees == 0 || rotationDegrees == 180) {
+            overlay.setImageSourceInfo(
+                imageProxy.width, imageProxy.height, isImageFlipped
+            )
+        } else {
+            overlay.setImageSourceInfo(
+                imageProxy.height, imageProxy.width, isImageFlipped
+            )
+        }
+        val frame = InputImage.fromMediaImage(
+            imageProxy.image!!,
+            imageProxy.imageInfo.rotationDegrees
+        )
+        objectDetector.process(frame)
+            .addOnSuccessListener { detectedObjects ->
+                // Task completed successfully
+                overlay.clear()
+                for (detectedObject in detectedObjects){
+                    val objGraphic = ObjectGraphic(this.overlay, detectedObject)
+                    this.overlay.add(objGraphic)
+                }
+                this.overlay.postInvalidate()
+
+            }
+
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+            }
+            .addOnCompleteListener {
+                imageProxy.close()
+
+            }
+
+
+    }
+
+}
+
